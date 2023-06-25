25950cd5a9df143c322ba165adb3b82508b49290
davidajimati <ajimatidavid@gmail.com>
2023-06-25 23:30:46 +0100

    - extracted search results from youtube  - converted the results to standard json format - written the response to a file at the 'frontend' directory

--- backend/search.js
+++ backend/search.js
@@ -1,16 +1,39 @@
+#!/usr/bin/env node
+// This module queries youTube based on title.
+
+const fs = require('fs');
 const { google } = require('googleapis');
 const youtube = google.youtube('v3');
+const filename = "../frontend/response.json"
+
 
 const apiKey = "AIzaSyDTposgVakSuuZgQG_Q0igZECJsNqWQnCY";
 
 youtube.search.list({
   key: apiKey, // or auth: auth for OAuth 2.0
   part: 'snippet',
+  q: "God will make a way - Don Meon",
+
+  "part": [
+    "snippet"
+  ],
+  "maxResults": 1,
   q: "God will make a way - Don Meon"
 }, (error, Response) => {
   if (error) {
     console.log("An error occurred", error);
     return;
+  } // check if request is successful
+  else if (Response.status === 200) {
+    console.log(Response.data.items)
+    const jsonData = JSON.stringify(Response.data.items)
+
+    //write results into a file
+    fs.writeFile(filename, jsonData, (err) => {
+      if (err) throw err;
+      else {
+        console.log("file written successfully.")
+      }
+    });
   }
-  console.log(Response);
 });
