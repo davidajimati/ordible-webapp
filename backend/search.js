fb59d2490e757e801760770fc4719fa0d7ed348f
davidajimati <ajimatidavid@gmail.com>
2023-06-26 10:31:30 +0100

    - API reesponse stored in JSON file - tested to overwrite exsiting content - videoresult folder changed to video-result

--- backend/search.js
+++ backend/search.js
@@ -4,33 +4,32 @@
 const fs = require('fs');
 const { google } = require('googleapis');
 const youtube = google.youtube('v3');
-const filename = "../frontend/response.json"
 
-
-const apiKey = "API_KEY_HERE";
+const filename = "../frontend/video-search/response.json"
+const apiKey = "AIzaSyDTposgVakSuuZgQG_Q0igZECJsNqWQnCY";
+const text = "We love the lord";
 
 youtube.search.list({
   key: apiKey, // or auth: auth for OAuth 2.0
-  part: 'snippet',
-  q: "God will make a way - Don Meon",
-
   "part": [
     "snippet"
   ],
-  "maxResults": 1,
-  q: "God will make a way - Don Meon"
+  "maxResults": 50,
+  q: text
 }, (error, Response) => {
   if (error) {
     console.log("An error occurred", error);
     return;
   } // check if request is successful
   else if (Response.status === 200) {
-    console.log(Response.data.items)
     const jsonData = JSON.stringify(Response.data.items)
 
     //write results into a file
-    fs.writeFile(filename, jsonData, (err) => {
-      if (err) throw err;
+    fs.writeFile(filename, jsonData, { flag: 'w+' }, err => {
+      if (err) {
+        console.log("There's an error", err);
+        return;
+      }
       else {
         console.log("file written successfully.")
       }
